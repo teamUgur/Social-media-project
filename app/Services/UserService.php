4d599e951b78544100428ff3ca5c77445958d13c
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-07-08 13:04:34 +0300

    private funcs

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -11,3 +11,36 @@ use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
+
+class UserService
+{
+    private function normilizeEmail(string $email) 
+    {
+        return strtolower($email);
+    }
+
+    private function defaultNameFromEmail(string $email) 
+    {
+        return explode("@", $email)[0];
+    }
+
+    private function getUserById(string $id) : User | null
+    {
+        return User::query()->where("id", $id)->first();
+    }
+
+    private function getUserByEmail(string $email) : User | null
+    {
+        return User::query()->where("email", $email)->first();
+    }
+
+    private function getUserByUsername(string $username) : User | null
+    {
+        return User::query()->where("username", $username)->first();
+    }
+
+    private function userMetaCacheKey(string $id) 
+    {
+        return "meta_user:$id";
+    }
+}
